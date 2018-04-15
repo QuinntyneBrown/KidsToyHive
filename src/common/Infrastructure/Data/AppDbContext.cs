649fdefb94491df693ac1f2b4a54e1e64b4eb381
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-04-14 20:57:13 -0400

    persistence

--- src/common/Infrastructure/Data/AppDbContext.cs
+++ src/common/Infrastructure/Data/AppDbContext.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Core.Entities;
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -26,6 +33,12 @@ namespace Infrastructure.Data
 
     public class AppDbContext: DbContext, IAppDbContext
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public AppDbContext(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public DbSet<Card> Cards { get; set; }
         public DbSet<Conversation> Conversations { get; set; }
         public DbSet<Contact> Contacts { get; set; }
@@ -40,5 +53,96 @@ namespace Infrastructure.Data
         public DbSet<ProductImage> ProductImages { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<User> Users { get; set; }
+
+        public Guid TenantId { get { return new Guid($"{this._httpContextAccessor.HttpContext.Items["TenantId"]}"); } }
+        public string CurrentUsername { get { return $"{this._httpContextAccessor.HttpContext.Items["CurrentUsername"]}"; } }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            ChangeTracker.DetectChanges();
+
+            foreach (var entity in ChangeTracker.Entries()
+                .Where(e => e.Entity is ILoggable && ((e.State == EntityState.Added || (e.State == EntityState.Modified))))
+                .Select(x => x.Entity as ILoggable))
+            {
+                var isNew = entity.CreatedOn == default(DateTime);
+                entity.CreatedOn = isNew ? DateTime.UtcNow : entity.CreatedOn;
+                entity.CreatedBy = isNew ? CurrentUsername : entity.CreatedBy;
+                entity.LastModifiedOn = DateTime.UtcNow;
+                entity.LastModifiedBy = CurrentUsername;
+            }
+
+            foreach (var item in ChangeTracker.Entries().Where(
+                e => e.State == EntityState.Added && e.Metadata.GetProperties().Any(p => p.Name == "TenantId")))
+            {
+                item.CurrentValues["TenantId"] = TenantId;
+            }
+
+            foreach (var item in ChangeTracker.Entries().Where(e => e.State == EntityState.Deleted))
+            {
+                item.State = EntityState.Modified;
+                item.CurrentValues["IsDeleted"] = true;
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        private static IList<Type> _entityTypeCache = default(IList<Type>);
+        private static IList<Type> GetEntityTypes()
+        {
+            if (_entityTypeCache != default(IList<Type>))
+            {
+                return _entityTypeCache.ToList();
+            }
+
+            _entityTypeCache = (from a in GetReferencingAssemblies()
+                                from t in a.DefinedTypes
+                                where t.BaseType == typeof(BaseEntity)
+                                select t.AsType()).ToList();
+
+            return _entityTypeCache;
+        }
+
+        private static IEnumerable<Assembly> GetReferencingAssemblies()
+        {
+            var assemblies = new List<Assembly>();
+            var dependencies = DependencyContext.Default.RuntimeLibraries;
+
+            foreach (var library in dependencies)
+            {
+                try
+                {
+                    var assembly = Assembly.Load(new AssemblyName(library.Name));
+                    assemblies.Add(assembly);
+                }
+                catch (FileNotFoundException)
+                { }
+            }
+            return assemblies;
+        }
+
+        static readonly MethodInfo SetGlobalQueryMethod = typeof(AppDbContext).GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                                                .Single(t => t.IsGenericMethod && t.Name == "SetGlobalQuery");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var type in GetEntityTypes())
+            {
+                var method = SetGlobalQueryMethod.MakeGenericMethod(type);
+                method.Invoke(this, new object[] { modelBuilder });
+            }
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Tenant>(mb =>
+            {
+                mb.Property(t => t.TenantId).HasDefaultValueSql("newsequentialid()");
+            });
+
+        }
+
+        public void SetGlobalQuery<T>(ModelBuilder builder) where T : BaseEntity
+        {
+            builder.Entity<T>()
+                .HasQueryFilter(e => EF.Property<Guid>(e, "TenantId") == TenantId && !e.IsDeleted);
+        }
+
     }
 }
