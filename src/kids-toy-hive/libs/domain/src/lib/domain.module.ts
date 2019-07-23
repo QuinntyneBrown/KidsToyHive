6a2499ce99293174e443eb1f39965ebd41aa723b
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-07-22 20:41:13 -0400

    add services

--- src/kids-toy-hive/libs/domain/src/lib/domain.module.ts
+++ src/kids-toy-hive/libs/domain/src/lib/domain.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AddressService, OrderService, DigitalAssetService } from './services';
+import { AddressService, OrderService, DigitalAssetService, CardService, CustomerService, DashboardCardService, DashboardService, InventoryItemService, OrderItemService, ProductCategoryService, ProductService, RoleService, UserService } from './services';
 import { HttpClientModule } from '@angular/common/http';
 
 export * from './services';
@@ -8,8 +8,18 @@ export * from './services';
 @NgModule({
   providers: [
     AddressService,
+    CardService,
+    CustomerService,
+    DashboardCardService,
+    DashboardService,
     DigitalAssetService,
-    OrderService
+    InventoryItemService,
+    OrderItemService,
+    OrderService,
+    ProductCategoryService,
+    ProductService,
+    RoleService,
+    UserService
   ],
   imports: [
     CommonModule,
