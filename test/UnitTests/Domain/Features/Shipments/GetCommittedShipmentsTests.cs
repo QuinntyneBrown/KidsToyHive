c75ef22c69f83fd0d8ac818e77a3b00301cdc33e
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-07-27 06:48:54 -0400

    update tests

--- test/UnitTests/Domain/Features/Shipments/GetCommittedShipmentsTests.cs
+++ test/UnitTests/Domain/Features/Shipments/GetCommittedShipmentsTests.cs
@@ -1,6 +1,8 @@
 using KidsToyHive.Api;
+using KidsToyHive.Core.Enums;
 using KidsToyHive.Domain.DataAccess;
 using KidsToyHive.Domain.Features.Shipments;
+using KidsToyHive.Domain.Models;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Moq;
@@ -11,6 +13,32 @@ namespace UnitTests.Domain.Features.Shipments
 {
     public class GetCommittedShipmentsTests
     {
+        [Fact]
+        public async Task ShouldGetCommittedShipmentsEmpty()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase($"{nameof(GetCommittedShipmentsTests)}:{nameof(ShouldGetCommittedShipmentsEmpty)}")
+                .Options;
+
+            var mediator = new Mock<IMediator>().Object;
+
+            using (var context = new AppDbContext(options, mediator))
+            {
+                SeedData.Seed(context, ConfigurationHelper.Seed);
+
+                var user = await context.Users.FirstAsync();
+
+                var getCommittedShipmentsHandler = new GetCommittedShipments.Handler(context);
+
+                var result = await getCommittedShipmentsHandler.Handle(new GetCommittedShipments.Request
+                {
+                    CurrentUsername = user.Username
+                }, default);
+
+                Assert.Empty(result.Shipments);
+            }
+        }
+
         [Fact]
         public async Task ShouldGetCommittedShipments()
         {
@@ -24,15 +52,37 @@ namespace UnitTests.Domain.Features.Shipments
             {
                 SeedData.Seed(context, ConfigurationHelper.Seed);
 
+                var driver = await context.Drivers.FirstAsync();
+
+                context.Shipments.Add(new Shipment
+                {
+                    Status = ShipmentStatus.Committed
+                });
+
+                context.Shipments.Add(new Shipment
+                {
+                    DriverId = driver.DriverId,
+                    Status = ShipmentStatus.Committed
+                });
+
+                context.Shipments.Add(new Shipment
+                {
+                    DriverId = driver.DriverId,
+                    Status = ShipmentStatus.Completed
+                });
+
+                context.SaveChanges();
+
                 var user = await context.Users.FirstAsync();
 
                 var getCommittedShipmentsHandler = new GetCommittedShipments.Handler(context);
 
-                var result = await getCommittedShipmentsHandler.Handle(new GetCommittedShipments.Request {
+                var result = await getCommittedShipmentsHandler.Handle(new GetCommittedShipments.Request
+                {
                     CurrentUsername = user.Username
                 }, default);
 
-                Assert.Empty(result.Shipments);
+                Assert.Single(result.Shipments);
             }
         }
     }
