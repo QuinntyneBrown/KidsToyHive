3853a9c2c26ff53ccfd2d4baeb4246e109cb54fe
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-08-18 04:54:42 -0400

    update terms and conditions

--- src/KidsToyHive.Api/Migrations/20190818083141_InitialCreate.Designer.cs
+++ src/KidsToyHive.Api/Migrations/20190818083141_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KidsToyHive.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20190816100935_InitialCreate")]
+    [Migration("20190818083141_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
