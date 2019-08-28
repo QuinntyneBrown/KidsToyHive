076b11c99514db7216076b5848b7493b01bc3c57
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-08-27 23:23:16 -0400

    update

--- src/kids-toy-hive/apps/kids-toy-hive-drivers-app/src/app/app.component.ts
+++ src/kids-toy-hive/apps/kids-toy-hive-drivers-app/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { LocalStorageService, accessTokenKey } from '@kids-toy-hive/core';
 import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'kth-root',
@@ -13,11 +15,34 @@ export class AppComponent {
     private readonly _localStorageService: LocalStorageService,
     private readonly _router: Router
   ) {
-
+    this._localStorageService.changes$
+    .pipe(
+      tap(x => {
+          this.loggedIn = this._localStorageService.get({ name: accessTokenKey }) != null;
+      })
+    )
+    .subscribe();
   }
+
+  @ViewChild(MatSidenav, { static: false })
+  public drawer:MatSidenav;
 
   public tryToLogout() {
     this._localStorageService.remove({ name: accessTokenKey });
     this._router.navigateByUrl('/login');
+  }
+
+  private _loggedIn:boolean;
+
+  public get loggedIn():boolean {
+    return this._loggedIn;
+  }
+
+  public set loggedIn(value:boolean) {
+
+    if(value !== this._loggedIn && this.drawer) 
+      this.drawer.close();
+
+    this._loggedIn = value;
   }
 }
