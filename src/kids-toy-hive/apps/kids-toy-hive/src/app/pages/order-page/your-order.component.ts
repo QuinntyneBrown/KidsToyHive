7da8cf8102718fab1558a841dd99e40aeb24fadd
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-08-28 20:05:22 -0400

    clean up

--- src/kids-toy-hive/apps/kids-toy-hive/src/app/pages/order-page/your-order.component.ts
+++ src/kids-toy-hive/apps/kids-toy-hive/src/app/pages/order-page/your-order.component.ts
@@ -1,30 +1,17 @@
-import { Component, OnDestroy, Input, Inject } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, Input } from '@angular/core';
 import { Product } from '@kids-toy-hive/domain';
-import { baseUrl } from '@kids-toy-hive/core';
 import { YourOrderService } from './your-order.service';
-
 
 @Component({
   templateUrl: './your-order.component.html',
   styleUrls: ['./your-order.component.css'],
   selector: 'kth-your-order'
 })
-export class YourOrderComponent implements OnDestroy  { 
-  public onDestroy: Subject<void> = new Subject<void>();
+export class YourOrderComponent {   
+  constructor(public readonly yourOrderService: YourOrderService) { }
 
-  constructor(
-    @Inject(baseUrl) private readonly _baseUrl:string,
-    public readonly yourOrderService: YourOrderService
-  ) {
-
-  }
   @Input()
   public product: Product;
-
-  ngOnDestroy() {
-    this.onDestroy.next();	
-  }
 
   public get price():number {
     if(this.yourOrderService.bookingTimeSlot$.value > 1) {
@@ -33,7 +20,4 @@ export class YourOrderComponent implements OnDestroy  {
       return 135;
     }
   }
-
-  public get baseUrl() { return this._baseUrl; }
-
 }
